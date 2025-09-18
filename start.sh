b15ed7d03fff9d7ef1d1dc86c5ae6967ceae8504
CubingHater <mac1110971@icloud.com>
2025-09-18 17:53:29 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "Installing dependencies..."
-pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
 echo "Starting Discord bot..."
 python3 florrcalc.py
+
 
